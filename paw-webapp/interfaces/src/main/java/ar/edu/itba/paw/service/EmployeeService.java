370af81fd64c2632e33b387c2ea33154fb637054
florencia chao <fchao@itba.edu.ar>
2022-04-08 11:59:05 -0300

    Employee services y su implementacion

---
+++ paw-webapp/interfaces/src/main/java/ar/edu/itba/paw/service/EmployeeService.java
@@ -0,0 +1,10 @@
+package ar.edu.itba.paw.service;
+
+import ar.edu.itba.paw.model.Employee;
+
+import java.util.Optional;
+
+public interface EmployeeService {
+    Optional<Employee> getEmployeeById(long id);
+    Employee create(String name, String location, long id, String availability);
+}
