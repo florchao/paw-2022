37ad985440c0a3aa7f5f734b0739f0239d313b9f
florencia chao <fchao@itba.edu.ar>
2022-04-16 17:00:01 -0300

    detalles del login - falta hacer el redireccionamiento del init cuando estas con sesion iniciada

--- paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/auth/PawUserDetailsService.java
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/auth/PawUserDetailsService.java
@@ -8,17 +8,24 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Optional;
+import java.util.regex.Pattern;
 
 @Component
 public class PawUserDetailsService implements UserDetailsService {
     @Autowired
-    private UserService us
-            ;
+    private UserService us;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    private Pattern BCRYPT_PATTERN = Pattern.compile("\\A\\$2a?\\$\\d\\d\\$[./0-9A-Za-z]{53}");
+
     @Override
     public UserDetails loadUserByUsername(final String username)
             throws UsernameNotFoundException {
@@ -26,9 +33,21 @@ public class PawUserDetailsService implements UserDetailsService {
         if (user == null) {
             throw new UsernameNotFoundException("No user by the name " + username);
         }
+       // if(!BCRYPT_PATTERN.matcher(user.get().getPassword()).matches()){
+            //TODO update user password in db
+        //}
+
+        //Es para usuarios que no tienen contraseña hasheada
+        String password = user.get().getPassword();
+        if(!BCRYPT_PATTERN.matcher(password).matches()){
+            //TODO update user password in db
+            password = passwordEncoder.encode(password);
+        }
+
+
             final Collection<? extends GrantedAuthority> authorities = Arrays.asList(
                     new SimpleGrantedAuthority("ROLE_USER"),
                     new SimpleGrantedAuthority("ROLE_ADMIN"));
-            return new org.springframework.security.core.userdetails.User(username, user.get().getPassword(), authorities);
+            return new org.springframework.security.core.userdetails.User(username, password, authorities);
         }
     }
--- paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebAuthConfig.java
@@ -37,13 +37,13 @@ public class WebAuthConfig extends WebSecurityConfigurerAdapter {
         http.sessionManagement()
                 .invalidSessionUrl("/login")
                 .and().authorizeRequests()
-                .antMatchers("/login").anonymous()
+                .antMatchers("/login", "/").anonymous()
                 .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/**").authenticated()
+                .antMatchers("/**" ).authenticated()
                 .and().formLogin()
                 .usernameParameter("j_username")
                 .passwordParameter("j_password")
-                .defaultSuccessUrl("/", false)
+                .defaultSuccessUrl("/buscarEmpleadas", false)
                 .loginPage("/login")
                 .and().rememberMe()
                 .rememberMeParameter("j_rememberme")
@@ -61,7 +61,7 @@ public class WebAuthConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(final WebSecurity web) throws Exception {
         web.ignoring()
-                .antMatchers("/css/**", "/js/**", "/img/**", " /favicon.ico", "/403");
+                .antMatchers("/css/**", "/js/**", "/img/**", " /favicon.ico", "/403", "/public/**");
     }
 
 }
