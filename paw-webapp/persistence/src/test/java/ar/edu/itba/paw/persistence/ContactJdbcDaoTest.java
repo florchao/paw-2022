d63f150347016479038db2bf98d0c9ec3608b6ea
florencia chao <fchao@itba.edu.ar>
2022-05-08 21:10:01 -0300

    tests job jdbc dao

---
+++ paw-webapp/persistence/src/test/java/ar/edu/itba/paw/persistence/ContactJdbcDaoTest.java
@@ -0,0 +1,54 @@
+package ar.edu.itba.paw.persistence;
+
+import ar.edu.itba.paw.model.Contact;
+import ar.edu.itba.paw.model.Job;
+import com.sun.xml.internal.bind.v2.model.core.ID;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.jdbc.JdbcTestUtils;
+
+import javax.sql.DataSource;
+import java.sql.Date;
+import java.util.List;
+import java.util.Optional;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TestConfig.class)
+@Sql("classpath:schema.sql")
+public class ContactJdbcDaoTest {
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    private ContactJdbcDao contactJdbcDao;
+
+    private JdbcTemplate jdbcTemplate;
+
+    private static final long EMPLOYEE_ID = 1;
+    private static final long EMPLOYER_ID = 1;
+    private static final Date DATE = new Date(10);
+    private static final String MESSAGE = "Message";
+    private static final String PHONE = "Phone";
+
+    @Test
+    public void testCreate(){
+        Contact contact = contactJdbcDao.create(EMPLOYEE_ID, EMPLOYER_ID, DATE,MESSAGE, PHONE );
+
+        Assert.assertNotNull(contact);
+        Assert.assertEquals(EMPLOYEE_ID, contact.getEmployeeID());
+        Assert.assertEquals(EMPLOYER_ID, contact.getEmployerID());
+        Assert.assertEquals(DATE, contact.getCreated());
+        Assert.assertEquals(MESSAGE, contact.getMessage());
+        Assert.assertEquals(PHONE, contact.getPhoneNumber());
+        Assert.assertEquals(1, JdbcTestUtils.countRowsInTable(jdbcTemplate, "contact"));
+
+    }
+
+
+}
