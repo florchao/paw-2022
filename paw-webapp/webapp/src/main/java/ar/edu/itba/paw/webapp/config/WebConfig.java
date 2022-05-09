5d3d750db25dff69ae602584661a7ef7bda8776d
sol <skonfederak@itba.edu.ar>
2022-05-09 20:36:33 -0300

    contraseñas ya no figuran en web config

--- paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -6,7 +6,9 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
@@ -16,6 +18,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
@@ -29,7 +32,14 @@ import java.util.Properties;
 @ComponentScan({"ar.edu.itba.paw.webapp.controller", "ar.edu.itba.paw.service", "ar.edu.itba.paw.persistence"})
 @Configuration
 @EnableAsync
-public class WebConfig {
+@PropertySource(value= {"classpath:application.properties"})
+
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    private static final boolean DEV_BUILD = false; // Change this to a config/profile in the future
+    private static boolean isOnDevBuild() {
+        return DEV_BUILD;
+    }
 
     @Value("classpath:schema.sql")
     private Resource schemaSql;
@@ -44,37 +54,6 @@ public class WebConfig {
 
         return viewResolver;
     }
-
-    @Bean
-    public DataSource dataSource() {
-        final SimpleDriverDataSource ds = new SimpleDriverDataSource();
-
-        ds.setDriverClass(org.postgresql.Driver.class);
-        ds.setUrl("jdbc:postgresql://localhost/paw");
-        ds.setUsername("root");
-        ds.setPassword("root");
-        return ds;
-    }
-
-    @Bean
-    public Session mailSession() {
-        Properties props = new Properties();
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.socketFactory.port", "465");
-        props.put("mail.smtp.socketFactory.class",
-                "javax.net.ssl.SSLSocketFactory");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.port", "465");
-        Session session = Session.getDefaultInstance(props,
-                new javax.mail.Authenticator() {
-                    @Override
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication("hogarempleos22@gmail.com", "paw202202");
-                    }
-                });
-        return session;
-    }
-
     @Bean
     public DataSourceInitializer dataSourceInitializer(final DataSource ds) {
         final DataSourceInitializer dsi = new DataSourceInitializer();
@@ -87,6 +66,44 @@ public class WebConfig {
         final ResourceDatabasePopulator dbp = new ResourceDatabasePopulator();
         dbp.addScript(schemaSql);
         return dbp;
+    }
+    @Autowired
+    Environment environment;
+
+    @Bean
+    public DataSource dataSource() {
+        final SimpleDriverDataSource ds = new SimpleDriverDataSource();
+        ds.setDriverClass(org.postgresql.Driver.class);
+
+        if (isOnDevBuild()) {
+            ds.setUrl(environment.getRequiredProperty("db.dev.url"));
+            ds.setUsername(environment.getRequiredProperty("db.dev.username"));
+            ds.setPassword(environment.getRequiredProperty("db.dev.password"));
+        } else {
+            ds.setUrl(environment.getRequiredProperty("db.prod.url"));
+            ds.setUsername(environment.getRequiredProperty("db.prod.username"));
+            ds.setPassword(environment.getRequiredProperty("db.prod.password"));
+        }
+
+        return ds;
+    }
+
+    @Bean
+    public Session mailSession() {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class",
+                "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+        return Session.getDefaultInstance(props,
+                new javax.mail.Authenticator() {
+                    @Override
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(environment.getRequiredProperty("mail.username"), environment.getRequiredProperty("mail.password"));
+                    }
+                });
     }
 
     @Bean
