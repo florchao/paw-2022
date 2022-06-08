b935a4f11400f7db54b80081b3b5b2b6daffd807
florencia chao <fchao@itba.edu.ar>
2022-06-08 19:59:33 -0300

    cambios de persistencia y servicio - listo review

--- paw-webapp/interfaces/src/main/java/ar/edu/itba/paw/persistence/ReviewDao.java
+++ paw-webapp/interfaces/src/main/java/ar/edu/itba/paw/persistence/ReviewDao.java
@@ -1,17 +1,20 @@
 package ar.edu.itba.paw.persistence;
 
+import ar.edu.itba.paw.model.Employee;
+import ar.edu.itba.paw.model.Employer;
 import ar.edu.itba.paw.model.Review;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ReviewDao {
-    Review create(long employeeId, long employerId, String review);
+    Review create(Employee employeeId, Employer employerId, String review);
 
-    List<Review> getAllReviews(long employeeId, Long id, Long page, int pageSize);
+    List<Review> getAllReviews(Employee employeeId, Employer id, Long page, int pageSize);
 
-    int getPageNumber(long employeeId, Long id, int pageSize);
+    int getPageNumber(Employee employeeId, Employer id, int pageSize);
 
-    List<Review> getMyProfileReviews(long employeeId);
-    Optional<Review> getMyReview(long employeeId, long id);
+    List<Review> getMyProfileReviews(Employee employeeId);
+
+    Optional<Review> getMyReview(Employee employeeId, Employer id);
 }
--- paw-webapp/service/src/main/java/ar/edu/itba/paw/service/ReviewServiceImpl.java
+++ paw-webapp/service/src/main/java/ar/edu/itba/paw/service/ReviewServiceImpl.java
@@ -1,12 +1,16 @@
 package ar.edu.itba.paw.service;
 
+import ar.edu.itba.paw.model.Employee;
+import ar.edu.itba.paw.model.Employer;
 import ar.edu.itba.paw.model.Review;
+import ar.edu.itba.paw.persistence.EmployeeDao;
+import ar.edu.itba.paw.persistence.EmployerDao;
 import ar.edu.itba.paw.persistence.ReviewDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.swing.text.html.Option;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,33 +19,56 @@ public class ReviewServiceImpl implements ReviewService{
 
     @Autowired
     private ReviewDao reviewDao;
+    @Autowired
+    private EmployeeDao employeeDao;
+    @Autowired
+    private EmployerDao employerDao;
 
     @Transactional
     @Override
     public Review create(long employeeId, long employerId, String review) {
-        return reviewDao.create(employeeId, employerId, review);
+        Optional<Employee> employee = employeeDao.getEmployeeById(employeeId);
+        Optional<Employer> employer = employerDao.getEmployerById(employerId);
+        if (employee.isPresent() && employer.isPresent())
+            return reviewDao.create(employee.get(), employer.get(), review);
+        return null;
     }
 
     @Transactional(readOnly = true)
     @Override
     public List<Review> getAllReviews(long employeeId, Long id, Long page, int pageSize) {
-        return reviewDao.getAllReviews(employeeId, id, page, pageSize);
+        Optional<Employee> employee = employeeDao.getEmployeeById(employeeId);
+        Optional<Employer> employer = employerDao.getEmployerById(id);
+        if (employee.isPresent() && employer.isPresent())
+            return reviewDao.getAllReviews(employee.get(), employer.get(), page, pageSize);
+        return Collections.emptyList();
     }
 
     @Transactional(readOnly = true)
     @Override
     public int getPageNumber(long employeeId, Long id, int pageSize) {
-        return reviewDao.getPageNumber(employeeId, id, pageSize);
+        Optional<Employee> employee = employeeDao.getEmployeeById(employeeId);
+        Optional<Employer> employer = employerDao.getEmployerById(id);
+        if (employee.isPresent() && employer.isPresent())
+            return reviewDao.getPageNumber(employee.get(), employer.get(), pageSize);
+        return 0;
     }
 
     @Transactional(readOnly = true)
     @Override
     public Optional<Review> getMyReview(long employeeId, long id) {
-        return reviewDao.getMyReview(employeeId, id);
+        Optional<Employee> employee = employeeDao.getEmployeeById(employeeId);
+        Optional<Employer> employer = employerDao.getEmployerById(id);
+        if (employee.isPresent() && employer.isPresent())
+            return reviewDao.getMyReview(employee.get(), employer.get());
+        return Optional.empty();
     }
 
     @Override
     public List<Review> getMyProfileReviews(long employeeId) {
-        return reviewDao.getMyProfileReviews(employeeId);
+        Optional<Employee> employee = employeeDao.getEmployeeById(employeeId);
+        if (employee.isPresent() )
+            return reviewDao.getMyProfileReviews(employee.get());
+        return Collections.emptyList();
     }
 }
