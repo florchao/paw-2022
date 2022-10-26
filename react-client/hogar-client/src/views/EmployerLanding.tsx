81598a1d0b18c8f4f7d00cd444e4ececf0ce3812
florencia chao <fchao@itba.edu.ar>
2022-10-26 17:52:37 -0300

    buttons redirect to corresponding pages

--- react-client/hogar-client/src/views/EmployerLanding.tsx
+++ react-client/hogar-client/src/views/EmployerLanding.tsx
@@ -23,7 +23,7 @@ export const EmployerLanding = () => {
             <div id="roles" className="grid grid-cols-2 items-start p-10 gap-y-7 mt-9 content-center">
                 <div className="grid grid-cols-3">
                     <div className="h-48 w-48">
-                        <img src={ './images/searchJob.jpeg'} alt="employer"/>
+                        <img className="object-cover w-full h-full rounded-full" src={ './images/searchJob.jpeg'} alt="employer"/>
                     </div>
                     <div className="col-span-2 ">
                         <h3 className="text-2xl text-purple-700 justify-self-center">
@@ -33,15 +33,13 @@ export const EmployerLanding = () => {
                             Elige de nuestra comunidad el perfil que encaje con lo que estes buscando
                         </p>
                         <div className="pb-4 grid col-start-2 col-span-2 mt-7">
-                                <button className="bg-violet-300 font-semibold hover:bg-yellow-300 shadow-lg text-violet-900 py-2 px-4 rounded-xl w-2/5 border-solid border-transparent	border-2 hover:border-purple-300">
-                                    Buscar empleadas
-                                </button>
+                            <Button name="Buscar Empleadas" link="/explore"/>
                         </div>
                     </div>
                 </div>
                 <div className="col-start-2 grid grid-cols-3">
                     <div className="h-48 w-48">
-                        <img src={ './images/joboffer.jpeg' } alt="job offer picture"/>
+                        <img className="object-cover w-full h-full rounded-full" src={ './images/joboffer.jpeg' } alt="job offer picture"/>
                     </div>
                     <div className="col-span-2">
                         <h3 className="text-2xl text-purple-700 justify-self-center">
@@ -51,9 +49,7 @@ export const EmployerLanding = () => {
                             Publique una oferta de trabajo y espere a que nuestra comunidad aplique al mismo
                         </p>
                         <div className="pb-4 grid col-start-2 col-span-2 mt-7">
-                                <button className="bg-violet-300 font-semibold hover:bg-yellow-300 shadow-lg text-violet-900 py-2 px-4 rounded-xl w-2/5 border-solid border-transparent	border-2 hover:border-purple-300">
-                                    Mis trabajos publicados
-                                </button>
+                            <Button name="Mis trabajos publicados" link="/myjobs"/>
                         </div>
                     </div>
                 </div>
