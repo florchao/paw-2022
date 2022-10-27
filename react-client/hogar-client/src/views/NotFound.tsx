8a79839e3f9884e87cf029a04aff66c7aac18d72
sol <skonfederak@itba.edu.ar>
2022-10-27 17:49:28 -0300

    mejorando estilo

--- react-client/hogar-client/src/views/NotFound.tsx
+++ react-client/hogar-client/src/views/NotFound.tsx
@@ -9,7 +9,9 @@ const NotFound = () => {
                         <img src={ './images/warning2.png' } alt="Error" className="mr-3 h-6 sm:h-52"/>
                         <p className="text-3xl font-semibold text-purple-700">404 : Resource not found</p>
                         <br/>
-                        <Button name="Go back to home page" link="/"/> 
+                        <div className="w-full ">
+                            <Button name="Go back to home page" link="/"/> 
+                        </div>
                         {/* <button type="button" className="text-lg w-full focus:outline-none text-purple-700 bg-yellow-300 hover:bg-yellow-200 font-small rounded-lg text-lg px-5 py-2.5" onclick="history.back()"><spring:message code="404.goBack"/></button> */}
                     </div>
                 </div>
