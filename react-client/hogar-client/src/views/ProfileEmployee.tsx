6226050ac61e89fcbe5bdfefc17d0028bbaa5b3b
arfernandez2000 <arfernandez@itba.edu.ar>
2022-11-07 18:52:07 -0300

    Rating de las empleadas

--- react-client/hogar-client/src/views/ProfileEmployee.tsx
+++ react-client/hogar-client/src/views/ProfileEmployee.tsx
@@ -50,15 +50,14 @@ export const ProfileEmployee = () => {
         }, []
     )
 
-    // useEffect(() => {
-    //         RatingService.getEmployeeRating(id).then(
-    //             (rsp) => {
-    //                 setRating(rsp)
-    //                 console.log(rating)
-    //             }
-    //         )
-    //     }, []
-    // )
+    useEffect(() => {
+            RatingService.getEmployeeRating(id).then(
+                (rsp) => {
+                    setRating(rsp)
+                }
+            )
+        }, []
+    )
 
     return (
       <div className="grid overflow-auto h-screen grid-cols-6">
@@ -100,69 +99,102 @@ export const ProfileEmployee = () => {
                                   CONNECT
                               </button>
                           </Link>
-                          {rating &&
+                          {rating && rating.count != 0 &&
                               <ul className="flex items-center gap-x-1">
-                                  {rating && rating >= 0.75 &&
+                                  {rating && rating.rating >= 0.75 &&
                                       <li>
-                                          <i className=" text-yellow-300 fa-lg fa fa-star"></i>
+                                          <svg aria-hidden="true" className="w-5 h-5 text-yellow-400"
+                                               fill="currentColor" viewBox="0 0 20 20"
+                                               xmlns="http://www.w3.org/2000/svg"><title>First star</title>
+                                              <path
+                                                  d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                                          </svg>
                                       </li>
                                   }
-                                  {rating && rating < 0.75 &&
+                                  {rating && rating.rating < 0.75 &&
                                       <li>
-                                          <i className="text-yellow-300 fa-lg fa fa-star-o"></i>
+                                          <svg aria-hidden="true" className="w-5 h-5 text-gray-300 dark:text-gray-500"
+                                               fill="currentColor" viewBox="0 0 20 20"
+                                               xmlns="http://www.w3.org/2000/svg"><title>First star</title>
+                                              <path
+                                                  d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                                          </svg>
                                       </li>
                                   }
-                                  {/*    </c:otherwise>*/}
-                                  {/*</c:choose>*/}
-                                  {/*<c:choose>*/}
-                                  {/*    <c:when test="${rating >= 1.75}">*/}
-                                  {/*        <li>*/}
-                                  {/*            <i className="text-yellow-300 fa fa-lg fa-star"></i>*/}
-                                  {/*        </li>*/}
-                                  {/*    </c:when>*/}
-                                  {/*    <c:otherwise>*/}
-                                  {/*        <li>*/}
-                                  {/*            <i className="text-yellow-300 fa fa-lg fa-star-o"></i>*/}
-                                  {/*        </li>*/}
-                                  {/*    </c:otherwise>*/}
-                                  {/*</c:choose>*/}
-                                  {/*<c:choose>*/}
-                                  {/*    <c:when test="${rating >= 2.75}">*/}
-                                  {/*        <li>*/}
-                                  {/*            <i className="text-yellow-300 fa fa-lg fa-star"></i>*/}
-                                  {/*        </li>*/}
-                                  {/*    </c:when>*/}
-                                  {/*    <c:otherwise>*/}
-                                  {/*        <li>*/}
-                                  {/*            <i className="text-yellow-300 fa-lg fa fa-star-o"></i>*/}
-                                  {/*        </li>*/}
-                                  {/*    </c:otherwise>*/}
-                                  {/*</c:choose>*/}
-                                  {/*<c:choose>*/}
-                                  {/*    <c:when test="${rating >= 3.75}">*/}
-                                  {/*        <li>*/}
-                                  {/*            <i className="text-yellow-300 fa-lg fa fa-star"></i>*/}
-                                  {/*        </li>*/}
-                                  {/*    </c:when>*/}
-                                  {/*    <c:otherwise>*/}
-                                  {/*        <li>*/}
-                                  {/*            <i className="text-yellow-300 fa-lg fa fa-star-o"></i>*/}
-                                  {/*        </li>*/}
-                                  {/*    </c:otherwise>*/}
-                                  {/*</c:choose>*/}
-                                  {/*<c:choose>*/}
-                                  {/*    <c:when test="${rating >= 4.75}">*/}
-                                  {/*        <li>*/}
-                                  {/*            <i className="text-yellow-300 fa-lg fa fa-star"></i>*/}
-                                  {/*        </li>*/}
-                                  {/*    </c:when>*/}
-                                  {/*    <c:otherwise>*/}
-                                  {/*        <li>*/}
-                                  {/*            <i className="text-yellow-300 fa-lg fa fa-star-o"></i>*/}
-                                  {/*        </li>*/}
-                                  {/*    </c:otherwise>*/}
-                                  {/*</c:choose>*/}
-                                  {/*<c:out value="(${voteCount})"/>*/}
+                                  {rating && rating.rating >= 1.75 &&
+                                      <svg aria-hidden="true" className="w-5 h-5 text-yellow-400"
+                                           fill="currentColor" viewBox="0 0 20 20"
+                                           xmlns="http://www.w3.org/2000/svg"><title>Second star</title>
+                                          <path
+                                              d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                                      </svg>
+                                  }
+                                  {rating && rating.rating < 1.75 &&
+                                      <li>
+                                          <svg aria-hidden="true" className="w-5 h-5 text-gray-300 dark:text-gray-500"
+                                               fill="#E5E7EB" viewBox="0 0 20 20"
+                                               xmlns="http://www.w3.org/2000/svg"><title>Second star</title>
+                                              <path
+                                                  d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                                          </svg>
+                                      </li>
+                                  }
+                                  {rating && rating.rating >= 2.75 &&
+                                      <svg aria-hidden="true" className="w-5 h-5 text-yellow-400"
+                                           fill="currentColor" viewBox="0 0 20 20"
+                                           xmlns="http://www.w3.org/2000/svg"><title>Third star</title>
+                                          <path
+                                              d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                                      </svg>
+                                  }
+                                  {rating && rating.rating < 2.75 &&
+                                      <li>
+                                          <svg aria-hidden="true" className="w-5 h-5 text-gray-300 dark:text-gray-500"
+                                               fill="currentColor" viewBox="0 0 20 20"
+                                               xmlns="http://www.w3.org/2000/svg"><title>Third star</title>
+                                              <path
+                                                  d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                                          </svg>
+                                      </li>
+                                  }
+                                  {rating && rating.rating >= 3.75 &&
+                                      <svg aria-hidden="true" className="w-5 h-5 text-yellow-400"
+                                           fill="currentColor" viewBox="0 0 20 20"
+                                           xmlns="http://www.w3.org/2000/svg"><title>Fourth star</title>
+                                          <path
+                                              d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                                      </svg>
+                                  }
+                                  {rating && rating.rating < 3.75 &&
+                                      <li>
+                                          <svg aria-hidden="true" className="w-5 h-5 text-gray-300 dark:text-gray-500"
+                                               fill='%23E5E7EB' viewBox="0 0 20 20"
+                                               xmlns="http://www.w3.org/2000/svg"><title>Fourth star</title>
+                                              <path
+                                                  d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                                          </svg>
+                                      </li>
+                                  }
+                                  {rating && rating.rating >= 4.75 &&
+                                      <svg aria-hidden="true" className="w-5 h-5 text-yellow-400"
+                                           fill="currentColor" viewBox="0 0 20 20"
+                                           xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title>
+                                          <path
+                                              d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                                      </svg>
+                                  }
+                                  {rating && rating.rating < 4.75 &&
+                                      <li>
+                                          <svg aria-hidden="true" className="w-5 h-5 text-gray-300 dark:text-gray-500" viewBox="0 0 20 20"
+                                               fill='%23E5E7EB'
+                                               xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title>
+                                              <path
+                                                  d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                                          </svg>
+                                      </li>
+                                  }
+                                  <p>({rating && rating.count})</p>
+
                               </ul>
                           }
                       </div>
