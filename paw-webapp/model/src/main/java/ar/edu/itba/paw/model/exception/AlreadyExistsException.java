136d29c78e6b3df45b1e624c46c25cc4ecf1b5b7
sol <skonfederak@itba.edu.ar>
2022-12-01 17:28:18 -0300

    cambio de excepciones a su causa

--- paw-webapp/model/src/main/java/ar/edu/itba/paw/model/exception/AlreadyExistsException.java
+++ paw-webapp/model/src/main/java/ar/edu/itba/paw/model/exception/AlreadyExistsException.java
@@ -1,6 +1,6 @@
 package ar.edu.itba.paw.model.exception;
 
-public class AlreadyExistsException extends Exception{
+public class AlreadyExistsException extends RuntimeException{
     public AlreadyExistsException(String message){
         super(message);
     }
