5bf03623afb0d11424337da021bfab23c835022f
sol <skonfederak@itba.edu.ar>
2022-12-03 14:15:59 -0300

    Llamadas a la api y manejo de controller basico

--- react-client/hogar-client/src/service/ApplicantService.tsx
+++ react-client/hogar-client/src/service/ApplicantService.tsx
@@ -28,4 +28,58 @@ export class ApplicantService{
                     throw error
                 })
     }
+    //TODO: ver que devuelve el response
+    public static async createApplicant(e: any, employeeId: number, jobId: number){
+        e.preventDefault();
+
+        const formData:any = new FormData();
+        formData.append("employeeId", employeeId)
+        formData.append("jobId", jobId)
+
+        return await fetch('http://localhost:8080/api/applicant/', {
+            method: 'POST',
+            headers: {},
+            body: formData
+        }).then((r) => r.text())
+        //TODO: habria que refreshear la pagina del trabajo
+    }
+
+    //TODO:ajustar params a correcion
+    public static async updateStatus(e:any, employeeId: number, jobId: number, status: number){
+        e.preventDefault();
+
+        const formData:any = new FormData();
+        formData.append("employeeId", employeeId)
+        formData.append("jobId", jobId)
+        formData.append("status", status)
+
+        // return await fetch('http://localhost:8080/api/employee/' + employeeId +'/'+ jobId , {
+        return await fetch('http://localhost:8080/api/applicant/', {
+            method: 'PUT',
+            headers: {},
+            body: formData
+        }).then((r) => r.text())
+        // TODO: despues de que volvio me tendria que refreshear la lista de aplicantes
+    }
+
+    public static async deleteApplication(e:any, employeeId: number, jobId: number){
+        //TODO: ver como se pasan los ids
+        e.preventDefault();
+
+        const formData:any = new FormData();
+        formData.append("employeeId", employeeId)
+        formData.append("jobId", jobId)
+
+        return await fetch('http://localhost:8080/api/applicant/', {
+            method: 'DELETE',
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                // 'Content-Type': 'application/json'
+            },
+            body: formData
+        })
+        //TODO: redirigir a trabajos aplicados
+    }
+
+
 }
