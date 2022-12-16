7da6429cb809e5511c15c3e00a1e0deb53381b65
hfinucci <hfinucci@itba.edu.ar>
2022-12-16 18:42:08 -0300

    VERY simple pagination for seeing your jobs' applicants

--- paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/JobController.java
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/JobController.java
@@ -118,7 +118,7 @@ public class JobController {
                 .collect(Collectors.toList());
         int pages = applicantService.getPageNumber(jobId, PAGE_SIZE);
         GenericEntity<List<ApplicantDto>> genericEntity = new GenericEntity<List<ApplicantDto>>(list){};
-        return Response.ok(genericEntity).header("X-Total-Count", pages).build();
+        return Response.ok(genericEntity).header("Access-Control-Expose-Headers", "X-Total-Count") .header("X-Total-Count", pages).build();
     }
 
 
