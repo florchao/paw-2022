c76629275403ce11c8b61c5c97f2b0ac048bc885
florencia chao <fchao@itba.edu.ar>
2022-12-17 12:07:23 -0300

    cache delete on sumbit

--- react-client/hogar-client/src/views/EditEmployee.tsx
+++ react-client/hogar-client/src/views/EditEmployee.tsx
@@ -10,7 +10,7 @@ const EditEmployee = () => {
 
     const handleSubmit = async (data: any, e: any, image:File) => {
         const edit = await EmployeeService.editEmployee(e, self, data.name, data.location, data.experienceYears, data.hourlyFee, data.availabilities, data.abilities, image!)
-        localStorage.removeItem("employeeForm")
+        localStorage.removeItem("employeeEditForm")
         localStorage.removeItem("imgEmployeeForm")
 
         nav('/profile', {replace: true, state: {id: edit, status: -1}})
