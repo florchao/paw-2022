a2c652e51eb6e8c4888d0e4d0c83550dc6b8e4e1
florencia chao <fchao@itba.edu.ar>
2022-12-17 13:35:14 -0300

    logo navbar fixed when not logged in

--- react-client/hogar-client/src/components/Navbar.tsx
+++ react-client/hogar-client/src/components/Navbar.tsx
@@ -7,8 +7,8 @@ export const Navbar = () => {
     return (
         <nav className="bg-white w-full pr-2 sm:px-4 py-2.5 fixed shadow-md rounded z-10" style={{backgroundColor: "#ac70ff"}}>
             <div className="flex flex-wrap justify-between mx-auto">
-                <Link to={localStorage.getItem('hogar-role') == "EMPLOYEE"? "/explore" : "/"} className="flex items-center">
-                    <img src={'./images/hogar.png'} alt="logo" className="mr-3 h-9"/>
+                <Link to={localStorage.getItem('hogar-role') === "EMPLOYEE"? "/explore" : "/"} className="flex items-center">
+                    <img src={'./images/hogar.png'} alt="logo" className={"mr-3 h-9"}/>
                     {/*<span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Hogar</span>*/}
                 </Link>
                 {/*<button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">*/}
@@ -23,21 +23,21 @@ export const Navbar = () => {
                                 <h1 className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">{t('Navbar.explore')}</h1>
                             </Link>
                         </li>
-                        {localStorage.getItem("hogar-role") && localStorage.getItem("hogar-role") == "EMPLOYEE" &&
+                        {localStorage.getItem("hogar-role") && localStorage.getItem("hogar-role") === "EMPLOYEE" &&
                             <li>
                                 <Link to="/jobs" state={{id: localStorage.getItem("hogar-uid")}}>
                                     <h1 className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">{t('Navbar.jobsApplied')}</h1>
                                 </Link>
                             </li>
                         }
-                        {localStorage.getItem("hogar-role") && localStorage.getItem("hogar-role") == "EMPLOYER" &&
+                        {localStorage.getItem("hogar-role") && localStorage.getItem("hogar-role") === "EMPLOYER" &&
                             <li>
                                 <Link to="/jobs" state={{id: localStorage.getItem("hogar-uid")}}>
                                     <h1 className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">{t('Navbar.jobsPosted')}</h1>
                                 </Link>
                             </li>
                         }
-                        {localStorage.getItem("hogar-role") && localStorage.getItem("hogar-role") == "EMPLOYEE" &&
+                        {localStorage.getItem("hogar-role") && localStorage.getItem("hogar-role") === "EMPLOYEE" &&
                             <li>
                                 <Link to="/contacts" state={{id: localStorage.getItem("hogar-uid")}} >
                                     <h1 className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">{t('Navbar.contacts')}</h1>
