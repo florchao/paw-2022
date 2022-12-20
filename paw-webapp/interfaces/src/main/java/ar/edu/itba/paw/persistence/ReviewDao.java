7d5fa4da23d27189b9c606ae529ffbf2e209e0c4
hfinucci <hfinucci@itba.edu.ar>
2022-12-20 10:36:39 -0300

    finished pagination for reviews

--- paw-webapp/interfaces/src/main/java/ar/edu/itba/paw/persistence/ReviewDao.java
+++ paw-webapp/interfaces/src/main/java/ar/edu/itba/paw/persistence/ReviewDao.java
@@ -15,19 +15,19 @@ public interface ReviewDao {
 
     int getPageNumber(Employee employeeId, Employer id, int pageSize);
 
-//    List<Review> getMyProfileReviews(Employee employeeId,long page, int pageSize);
-
     Optional<Review> getMyReview(Employee employeeId, Employer id);
 
     List<Review> getAllReviewsEmployer(Employee employeeId, Employer id, Long page, int pageSize);
 
     int getPageNumberEmployer(Employee employeeId, Employer id, int pageSize);
 
-    List<Review> getMyProfileReviewsEmployer(Employer employerId, long page, int pageSize);
-
     Optional<Review> getMyReviewEmployer(Employee employeeId, Employer id);
 
-    int getMyProfileReviewsEmployerPageNumber(Employer employerId, int pageSize);
+//    List<Review> getMyProfileReviews(Employee employeeId,long page, int pageSize);
+
+//    List<Review> getMyProfileReviewsEmployer(Employer employerId, long page, int pageSize);
+
+//    int getMyProfileReviewsEmployerPageNumber(Employer employerId, int pageSize);
 
 //    int getMyProfileReviewsPageNumber(Employee employee, int pageSize);
 }
