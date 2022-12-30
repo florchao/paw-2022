2dc6f7a26a20f5be96d873837b602a5b587592b4
arfernandez2000 <arfernandez@itba.edu.ar>
2022-12-30 12:57:16 -0300

    Crear review con Bean Validator (sin imagen)

--- paw-webapp/hogar-client/src/service/ReviewService.tsx
+++ paw-webapp/hogar-client/src/service/ReviewService.tsx
@@ -85,35 +85,41 @@ export class ReviewService {
 
     public static async postEmployerReview(e: any, employerId: number, message: string) {
         e.preventDefault();
-        const formData: any = new FormData();
-        formData.append("content", message)
-        formData.append("employeeId", localStorage.getItem("hogar-uid"))
-        formData.append("employerId", employerId)
-        formData.append("forEmployee", false)
+
+        const reviewFrom = JSON.stringify({
+            employerId: employerId,
+            employeeId: localStorage.getItem("hogar-uid"),
+            content: message,
+            forEmployee: false
+        })
 
         return await fetch(REVIEWS_URL, {
             method: 'POST',
             headers: {
+                'Content-Type': 'application/json',
                 'Authorization': 'Bearer ' + localStorage.getItem('hogar-jwt') as string
             },
-            body: formData
+            body: reviewFrom
         })
     }
 
     public static async postEmployeeReview(e: any, employeeId: number, message: string) {
         e.preventDefault();
-        const formData: any = new FormData();
-        formData.append("content", message)
-        formData.append("employeeId", employeeId)
-        formData.append("employerId", localStorage.getItem("hogar-uid"))
-        formData.append("forEmployee", true)
+
+        const reviewForm = JSON.stringify({
+            employeeId: employeeId,
+            employerId: localStorage.getItem("hogar-uid"),
+            content: message,
+            forEmployee: true
+        })
 
         return await fetch(REVIEWS_URL, {
             method: 'POST',
             headers: {
+                'Content-Type': 'application/json',
                 'Authorization': 'Bearer ' + localStorage.getItem('hogar-jwt') as string
             },
-            body: formData
+            body: reviewForm
         })
     }
 }
--- paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ReviewController.java
+++ paw-webapp/webapp/src/main/java/ar/edu/itba/paw/webapp/controller/ReviewController.java
@@ -2,11 +2,13 @@ package ar.edu.itba.paw.webapp.controller;
 
 import ar.edu.itba.paw.model.Review;
 import ar.edu.itba.paw.service.ReviewService;
+import ar.edu.itba.paw.webapp.dto.ReviewDto.ReviewCreateDto;
 import ar.edu.itba.paw.webapp.dto.ReviewDto.ReviewDto;
 import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.validation.Valid;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -26,28 +28,26 @@ public class ReviewController {
 
     @POST
     @Path("")
-    @Consumes(value = {MediaType.MULTIPART_FORM_DATA,})
-    public Response postJobReview(@FormDataParam("content") String message,
-                                  @FormDataParam("employeeId") Long employeeId,
-                                  @FormDataParam("employerId") Long employerId,
-                                  @FormDataParam("forEmployee") Boolean forEmployee) {
-        if (message.length() < 10 || message.length() > 100 ||
-                Objects.equals(employeeId, employerId) || Objects.isNull(employeeId) || Objects.isNull(employerId)
-                || Objects.isNull(forEmployee)) {
-            return Response.status(Response.Status.BAD_REQUEST).build();
-        }
-        Review review = reviewService.create(employeeId, employerId, message, new Date(), forEmployee);
+    @Consumes(value = {MediaType.APPLICATION_JSON,})
+    public Response postJobReview(@Valid ReviewCreateDto reviewCreateDto) {
+//        if (message.length() < 10 || message.length() > 100 ||
+//                Objects.equals(employeeId, employerId) || Objects.isNull(employeeId) || Objects.isNull(employerId)
+//                || Objects.isNull(forEmployee)) {
+//            return Response.status(Response.Status.BAD_REQUEST).build();
+//        }
+        Review review = reviewService.create(reviewCreateDto.getEmployeeId(), reviewCreateDto.getEmployerId(), reviewCreateDto.getContent(), new Date(), reviewCreateDto.getForEmployee());
         if (review == null) {
             return Response.status(Response.Status.CONFLICT).build();
         }
         ReviewDto reviewDto;
-        if (forEmployee)
+        if (reviewCreateDto.getForEmployee())
             reviewDto = ReviewDto.fromEmployeeReview(uriInfo, review);
         else
             reviewDto = ReviewDto.fromEmployerReview(uriInfo, review);
         GenericEntity<ReviewDto> genericEntity = new GenericEntity<ReviewDto>(reviewDto) {
         };
 
+        //TODO: devolver la URI no el objeto
         return Response.status(Response.Status.CREATED).entity(genericEntity).build();
     }
 
