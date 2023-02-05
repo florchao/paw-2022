93a703cb7ab4456648f5b99efa73754e11ffb4ce
sol <skonfederak@itba.edu.ar>
2023-02-05 17:47:33 -0300

    EmployeeService.test

--- paw-webapp/hogar-client/src/tests/mocks/index.tsx
+++ paw-webapp/hogar-client/src/tests/mocks/index.tsx
@@ -1,4 +1,45 @@
 import {LocalStorageMock} from "./LocalStorageMock";
+export const employee1 = {
+    "abilitiesArr": [
+        "Cooking",
+        "Pet Care"
+    ],
+    "availabilityArr": [
+        "Part Time"
+    ],
+    "experienceYears": 3,
+    "hourlyFee": 2,
+    "id": 1,
+    "image": "http://localhost:8080/images/1",
+    "location": "GBA West",
+    "name": "Sol Konfe",
+    "rating": 3.5,
+    "ratings": "http://localhost:8080/ratings/1",
+    "reviews": "http://localhost:8080/employees/1/reviews",
+    "self": "http://localhost:8080/employees/1",
+    "users": "http://localhost:8080/users/1"
+};
+
+export const employee2 ={
+    "abilitiesArr": [
+        "Cooking"
+    ],
+    "availabilityArr": [
+        "Full Time",
+        "Overnight"
+    ],
+    "experienceYears": 5,
+    "hourlyFee": 297,
+    "id": 9,
+    "image": "http://localhost:8080/images/9",
+    "location": "GBA West",
+    "name": "Tomas",
+    "rating": 2.875,
+    "ratings": "http://localhost:8080/ratings/9",
+    "reviews": "http://localhost:8080/employees/9/reviews",
+    "self": "http://localhost:8080/employees/9",
+    "users": "http://localhost:8080/users/9"
+};
 
 export function mockSuccesfulResponse(
     code: number,
--- paw-webapp/hogar-client/src/tests/services/EmployeeService.test.tsx
+++ paw-webapp/hogar-client/src/tests/services/EmployeeService.test.tsx
@@ -1,14 +1,29 @@
-import {mockSuccesfulResponse} from "../mocks";
+import {employee1, employee2, mockSuccesfulResponse} from "../mocks";
 import {EmployeeService} from "../../service/EmployeeService";
 
 describe("Fetch employees", () => {
     test("It should return the first page of explore", () => {
         const headers = new window.Headers();
         headers.set("Accept-Language", "english");
-        mockSuccesfulResponse(200,{}, headers);
+        mockSuccesfulResponse(200,[employee1, employee1], headers);
         return EmployeeService.getEmployees('').then(response => {
             // @ts-ignore
             expect(response.status).toBe(200);
+            // @ts-ignore
+            expect(response.json().length).toBe(2);
         })
     });
 });
+
+describe("Fetch employee", () => {
+    test("It should return employee1", () => {
+        const headers = new window.Headers();
+        headers.set("Accept-Language", "english");
+        mockSuccesfulResponse(200, employee1, headers);
+        return EmployeeService.getEmployee("http://localhost:8080/employees/1", false).then(response => {
+            // @ts-ignore
+            expect(response).toStrictEqual(employee1)
+        })
+    });
+});
+
